489c1490dd7f3b789e5ead765d3792f873c03130
Shadowsith <shadowsith94@gmail.com>
2020-01-27 22:49:03 +0100

    add sqlite service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage';
-import { StorageService } from './service/storage.service';
 
 @NgModule({
   declarations: [AppComponent],
@@ -25,7 +24,6 @@ import { StorageService } from './service/storage.service';
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    StorageService,
   ],
   bootstrap: [AppComponent]
 })
