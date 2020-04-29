c0a6935cc548d9748fa33f7a04a3df2a3dede262
Shadowsith <shadowsith94@gmail.com>
2020-04-29 20:54:14 +0200

    update/fixed package.json

--- src/app/page/home/home.page.ts
+++ src/app/page/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { TAFFY } from 'taffy';
 
 @Component({
   selector: 'app-home',
@@ -11,17 +10,7 @@ export class HomePage implements OnInit {
   constructor() {}
 
   async ngOnInit(): Promise<void> {
-    const stocks = TAFFY([
-      {
-        "stock": "TSLA",
-        "price": 600
-      },
-      {
-        "stock": "AMD",
-        "price": 40.5
-      }
-    ]);
-    console.log(db.get());
+
   }
 
 }
