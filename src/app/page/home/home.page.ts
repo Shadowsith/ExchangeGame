0aa0d904d5a7627b2fc9f1baee3ed34a7a3cd9d2
Shadowsith <shadowsith94@gmail.com>
2020-04-29 20:27:22 +0200

    update npm packages, small changes in home and storage service

--- src/app/page/home/home.page.ts
+++ src/app/page/home/home.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TAFFY } from 'taffy';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor() {}
+
+  async ngOnInit(): Promise<void> {
+    const stocks = TAFFY([
+      {
+        "stock": "TSLA",
+        "price": 600
+      },
+      {
+        "stock": "AMD",
+        "price": 40.5
+      }
+    ]);
+    console.log(db.get());
+  }
 
 }
--- src/app/service/storage.service.ts
+++ src/app/service/storage.service.ts
@@ -1,6 +1,25 @@
 import { Storage } from '@ionic/storage';
 
 export class StorageService {
+    public depot = {};
+    public settings = {};
+
     constructor(private storage: Storage) {
+    }
+
+    public async getSettings(): Promise<any> {
+        return await this.get("settings");
+    }
+
+    public setSettings(value: any): void {
+        this.set("settings", value);
+    }
+
+    public async get(key: string): Promise<any> {
+        return await this.storage.get(key);
+    }
+
+    public set(key: string, value: any): void {
+        this.storage.set(key, value);
     }
 }
