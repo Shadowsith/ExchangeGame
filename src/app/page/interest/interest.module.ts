c0ced2a6c89006a54c9ba5e6e7b3764468c3405e
Shadowsith <shadowsith94@gmail.com>
2020-05-01 01:00:39 +0200

    fixed bug in component

--- src/app/page/interest/interest.module.ts
+++ src/app/page/interest/interest.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
-
 import { InterestPage } from './interest.page';
+import { AddInterestComponent } from 'src/app/component/add-intereset/add-interest.page';
 
 @NgModule({
   imports: [
@@ -18,6 +18,7 @@ import { InterestPage } from './interest.page';
       }
     ])
   ],
-  declarations: [InterestPage]
+  entryComponents: [AddInterestComponent],
+  declarations: [InterestPage, AddInterestComponent]
 })
 export class InterestPageModule {}
