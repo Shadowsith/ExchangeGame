82916494de7b42dc06f919c103d9f855edf522e2
Shadowsith <shadowsith94@gmail.com>
2020-05-02 01:03:58 +0200

    optimize ui, add message after successful settings save

--- src/app/page/settings/settings.page.ts
+++ src/app/page/settings/settings.page.ts
@@ -1,6 +1,7 @@
 import { Component  } from '@angular/core';
 import { App } from 'src/app/service/app.service';
 import { Tables } from 'src/app/service/database.service';
+import { AlertService } from 'src/app/service/alert.service';
 
 @Component({
   selector: 'app-settings',
@@ -8,10 +9,12 @@ import { Tables } from 'src/app/service/database.service';
   styleUrls: ['settings.page.scss']
 })
 export class SettingsPage {
+  public alert: AlertService;
   public apikey: string = '';
 
   constructor() {
     this.apikey = App.db.getApiKey();
+    this.alert = new AlertService();
   }
 
   public save() {
@@ -19,5 +22,6 @@ export class SettingsPage {
       { name: 'apikey', value: this.apikey });
     App.api.setApiKey(this.apikey);
     App.db.save();
+    this.alert.showMsg('Meldung', '', 'Die Einstellungen wurden gespeichert.', 'OK');
   }
 }
